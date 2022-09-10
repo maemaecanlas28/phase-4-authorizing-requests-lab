a1b1b3524b1e8c1ecef174b7f82530e9c891ac07
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-09-09 22:04:14 -0700

    DONE

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
 
   def show
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
     user = User.find(session[:user_id])
     render json: user
   end
+
+  def index
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    render json: User.all
 
 end
